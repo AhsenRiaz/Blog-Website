e8b2909748a7e767d4e3be2f46563f79bfb080ce
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-01-07 20:27:08 +0500

    Authentication Functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,20 @@ module.exports = {
       
     },
     `@contentful/gatsby-transformer-contentful-richtext`,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.apiKey ,
+          authDomain: process.env.authDomain,
+          databaseURL: process.env.databaseURL ,
+          projectId: process.env.projectId,
+          storageBucket: process.env.storageBucket,
+          messagingSenderId: process.env.messagingSenderId ,
+          appId: process.env.appId ,
+        }
+      }
+    }
   ],
 };
 
