df3f783567e6e6fa4367f611635423de3f99a8fd
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-01-07 21:28:20 +0500

    update

--- src/features/SignUp.tsx
+++ src/features/SignUp.tsx
@@ -1,15 +1,16 @@
 import firebase from "firebase"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCObRoKq4mq_OucM2zsu2WsfzS1z5RTNFU",
-    authDomain: "blogappbyahsen.firebaseapp.com",
-    projectId: "blogappbyahsen",
-    storageBucket: "blogappbyahsen.appspot.com",
-    messagingSenderId: "28323700262",
-    appId: "1:28323700262:web:40b2b5686ded0555c310e7"
-  };
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCObRoKq4mq_OucM2zsu2WsfzS1z5RTNFU",
+  authDomain: "blogappbyahsen.firebaseapp.com",
+  projectId: "blogappbyahsen",
+  storageBucket: "blogappbyahsen.appspot.com",
+  messagingSenderId: "28323700262",
+  appId: "1:28323700262:web:40b2b5686ded0555c310e7"
+})
+
   
   const auth = firebase.auth();
 
